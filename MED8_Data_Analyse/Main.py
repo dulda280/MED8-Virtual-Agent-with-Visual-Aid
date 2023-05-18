e3e66c4b0727c0f061a3599b64e603571fd96fa6
reptinias <k.spar@hotmail.com>
2023-05-18 09:54:37 +0200

    Update to the SUS script

--- MED8_Data_Analyse/Main.py
+++ MED8_Data_Analyse/Main.py
@@ -1,15 +1,12 @@
-from scipy import stats
 import numpy as np
 import pandas as pd
 import math
-import statsmodels
 import matplotlib.pyplot as plt
-import glob
-import os
-import pathlib
 
 df = pd.read_csv("System usability scale(SUS).csv")
-df.drop(["Tidsstempel","Participant ID"], axis='columns', inplace=True)
+df.drop(["Tidsstempel", "Participant ID"], axis='columns', inplace=True)
+
+participantAmount = 10
 
 susScew = ["positiv",
            "negativ",
@@ -25,20 +22,14 @@ susScew = ["positiv",
 def Average(lst):
     return sum(lst) / len(lst)
 
-def calculateMean(data):
-    averageData = []
-    tData = np.asarray(data).T.tolist()
-    for list in tData:
-        avg = Average(list)
-        averageData.append(round(avg, 2))
-    return averageData
-
 def calculateStd(data):
-    tempList = []
-    tData = np.asarray(data).T.tolist()
-    for list in tData:
-        tempList.append(round(math.sqrt(Average(list)), 2))
-    return tempList
+    avg = Average(data)
+    disSum = 0
+
+    for point in data:
+        disSum += math.pow(abs(point - avg), 2)
+
+    return math.sqrt(disSum/len(data))
 
 def mapLikert(row):
     mulNumber = 2.5
@@ -59,10 +50,12 @@ for index, row in df.iterrows():
     mappedSUS.append(mapLikert(row))
 
 print(mappedSUS)
-print(sum(mappedSUS)/10)
+print(Average(mappedSUS))
+print(calculateStd(mappedSUS))
+print(max(mappedSUS), min(mappedSUS))
 
 fig = plt.figure()
-plt.bar(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"], mappedSUS)
+plt.bar(range(1, participantAmount+1), mappedSUS)
 
 plt.xlabel("Participant")
 plt.ylabel("SUS Score")
